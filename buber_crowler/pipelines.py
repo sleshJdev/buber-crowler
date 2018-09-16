04886638f383f57685217b9c3edc5fa16eb9a008
Eugene <slesh.eugene93@gmail.com>
2018-09-16 03:41:31 +0300

    enable pipeline to filter ads without phone suffix and extract full phone

--- buber_crowler/pipelines.py
+++ buber_crowler/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+
+from scrapy.exceptions import DropItem
 
 
 class BuberCrowlerPipeline(object):
     def process_item(self, item, spider):
+        title = item['title']
+        phoneSuffix = item['phoneSuffix']
+        if phoneSuffix not in title:
+            raise DropItem("Ad {} doesn't contain phone suffix {} in title {}.".format(item['url'], phoneSuffix, title))
+
+        url = item['url']
+        pattern = '((?:\d+_)+{})'.format(phoneSuffix)
+        item['phone'] = re.search(pattern, url).group().replace('_', '-')
         return item
