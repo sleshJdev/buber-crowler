9b67b81d7deb0c8f8750510309e856d701b26c97
Eugene <slesh.eugene93@gmail.com>
2018-09-16 03:41:38 +0300

    extracted item classes to appropriate module

--- buber_crowler/items.py
+++ buber_crowler/items.py
@@ -6,9 +6,35 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
 
 
-class BuberCrowlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class AdItem(scrapy.Item):
+    id = scrapy.Field()
+    price = scrapy.Field()
+    title = scrapy.Field()
+    city = scrapy.Field()
+    age = scrapy.Field()
+    name = scrapy.Field()
+    ethnicity = scrapy.Field()
+    availability = scrapy.Field()
+    url = scrapy.Field()
+    tagline = scrapy.Field()
+    description = scrapy.Field()
+    phone = scrapy.Field()
+    phoneSuffix = scrapy.Field()
+
+
+class AdItemLoader(ItemLoader):
+
+    def __init__(self):
+        super().__init__(AdItem())
+
+    default_output_processor = TakeFirst()
+    age_in = MapCompose(int)
+    price_in = MapCompose(int)
+    description_in = MapCompose(str.strip)
+    description_out = Join(u'\n')
+    tagline_in = MapCompose(str.strip)
+    tagline_out = Join(u'\n')
